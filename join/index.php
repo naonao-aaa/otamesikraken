1b09433fb7e1d303435cfe01f6c3970616033f0b
Nao <naonao1814naonao@gmail.com>
2021-02-16 22:02:08 +0900

    画像ファイルのみをアップロードできる機能

--- join/index.php
+++ join/index.php
@@ -14,6 +14,14 @@ if(!empty($_POST)){
  if($_POST['password'] === ''){
 	 $error['password'] = 'blank';
  }
+ $fileName = $_FILES['image']['name'];
+ if(!empty($fileName)){
+    $ext = substr($fileName, -3);
+		if($ext != 'jpg' && $ext != 'gif' && $ext != 'png'){
+			$error['image'] = 'type';
+		}
+ }
+
  if(empty($error)){
 	 $image = date('YmdHis').$_FILES['image']['name'];
 	 move_uploaded_file($_FILES['image']['tmp_name'],'../member_picture/'.$image);
@@ -75,6 +83,9 @@ if($_REQUEST['action'] == 'rewrite' && isset($_SESSION['join'])){
 		<dt>写真など</dt>
 		<dd>
         	<input type="file" name="image" size="35" value="test"  />
+					<?php if($error['image'] === 'type'): ?>
+          <p class="error">*写真などは「.gif」または「.jpg」「.png」の画像を指定してください</p>
+          <?php endif; ?>
         </dd>
 	</dl>
 	<div><input type="submit" value="入力内容を確認する" /></div>
